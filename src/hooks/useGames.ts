564e9f58299e07ddf3bdfcf69baa7b6cd9a1655f
Kachisax <militaryboy001@gmail.com>
2023-03-26 07:35:21 -0600

    refactor: Removed Duplicated styles

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,43 +2,49 @@ import { CanceledError } from "axios";
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 
-export interface Platform{
-    id: number;
-    name: string;
-    slug: string;
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: {platform: Platform }[];
-    metacritic: number;
-  }
-  
-  interface FetchGames {
-    count: number;
-    results: Game[];
-  }
-  
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+}
+
+interface FetchGames {
+  count: number;
+  results: Game[];
+}
 
 const useGames = () => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState("");
-  
-    useEffect(() => {
-const controller = new AbortController();
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-      apiClient
-        .get<FetchGames>("/games", {signal: controller.signal})
-        .then((res) => setGames(res.data.results))
-        .catch((err) => {if (err instanceof CanceledError) return; setError(err.message)});
+  useEffect(() => {
+    const controller = new AbortController();
 
-    return ()=> controller.abort()
-    },[]); 
+    setIsLoading(true);
+    apiClient
+      .get<FetchGames>("/games", { signal: controller.signal })
+      .then((res) => {
+        setGames(res.data.results);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
 
-    return {games, error}
+    return () => controller.abort();
+  }, []);
 
-}
- 
+  return { games, error, isLoading };
+};
+
 export default useGames;
